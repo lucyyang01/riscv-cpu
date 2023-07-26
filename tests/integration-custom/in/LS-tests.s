e913b719031dbf38f868815a92cbc9caffff7f94
Lucy Yang <lyang01@berkeley.edu>
2023-07-26 11:45:24 -0700

    pushing project need to add readme

---
+++ tests/integration-custom/in/LS-tests.s
@@ -0,0 +1,36 @@
+addi s0, x0, 200
+addi s1, x0, 1864
+
+sw   s1, 80(s0)
+lw   a0, 80(s0)
+
+sh   s1, 80(s0)
+lw   a0, 80(s0)
+sh   s1, 82(s0)
+lw   a0, 80(s0)
+
+sb   s1, 80(s0)
+lw   a0, 80(s0)
+sb   s1, 81(s0)
+lw   a0, 80(s0)
+sb   s1, 82(s0)
+lw   a0, 80(s0)
+sb   s1, 83(s0)
+lw   a0, 80(s0)
+
+sw   s1, -204(s0)
+lw   a0, -204(s0)
+
+sh   s1, -204(s0)
+lw   a0, -204(s0)
+sh   s1, -202(s0)
+lw   a0, -204(s0)
+
+sb   s1, -204(s0)
+lw   a0, -204(s0)
+sb   s1, -203(s0)
+lw   a0, -204(s0)
+sb   s1, -202(s0)
+lw   a0, -204(s0)
+sb   s1, -201(s0)
+lw   a0, -204(s0)
---
+++ tests/integration-custom/in/auipc-lui.s
@@ -0,0 +1,5 @@
+auipc t0 1
+auipc t1 1
+auipc t2 1
+
+lui t1 3
